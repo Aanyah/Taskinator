1221f4faf0fa5c7e97d404937379807c6a9ecee9
CRUMBB <aanyahlc@gmail.com>
2021-11-15 15:29:05 -0500

    form taskTypeInput correction

--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,6 @@ var tasksToDoEl= document.querySelector("#task-to-do");
 var createTaskHandler = function(event) {
 	event.preventDefault();
 	var taskNameInput = document.querySelector("input[name= 'task-name']").value;
-	console.dir(taskNameInput);
 	var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
 // create list item
@@ -16,7 +15,7 @@ var taskInfoEl = document.createElement("div");
 taskInfoEl.className = "task-info";
 
 // add HTML content to div
-taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>"+ taskNameInput + "</span>";
+taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>"+ taskTypeInput + "</span>";
 listItemEl.appendChild(taskInfoEl);
 
 // add entire list item to list
